a41580d49457328887cf3187c8d55edb2c6f3eab
Fenton Travers <fenton.travers@gmail.com>
2018-01-26 09:53:10 +0700

    adding in recom buttons

--- project.clj
+++ project.clj
@@ -5,10 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [reagent "0.7.0"]
-                 [hiccup "1.0.5"]
+                 [re-com "0.8.0"]
                  [org.clojure/clojurescript "1.9.946"]]
 
-  :source-paths ["src/cljs"]
+  :source-paths ["src/clj"]
 
   :plugins [[lein-cljsbuild "1.1.7"  :exclusions [[org.clojure/clojure]]]
             [lein-figwheel "0.5.13"]]
@@ -35,6 +35,5 @@
                              [figwheel-sidecar "0.5.13"]
                              [com.cemerick/piggieback "0.2.2"]
                              [org.clojure/tools.nrepl "0.2.13"]]
-
-              :plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]]
+              
               :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
--- src/cljs/sample_reagent/core.cljs
+++ src/cljs/sample_reagent/core.cljs
@@ -1,15 +1,35 @@
 (ns sample-reagent.core
   (:require
+   [re-com.core :as recom]
    [reagent.core :as reagent]))
 
 (defn some-component [ratom]
   [:div (:text @ratom)])
 
+(defn some1 []
+  [:div "abc"])
+
+(defn my-button []
+  [recom/h-box
+   :gap "10px"
+   :justify :around
+   :children
+   [[recom/button
+     :class "btn-error"
+     :tooltip "hello there "
+     :label "def"]
+    [recom/button
+     :class "btn-error"
+     :tooltip "hello there "
+     :label "abc"]]])
+
+;; (reagent/render-to-static-markup some1)
+
 (defonce app-state
   (reagent/atom {:text "Hello, what is your name??"}))
 
 (defn on-js-reload []
-  (reagent/render [some-component app-state]
+  (reagent/render [my-button]
             (.getElementById js/document "app")))
 
 (defn ^:export main []
